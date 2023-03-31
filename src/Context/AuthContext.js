30ae736b19c2788975dada4e74c516f67b272a77
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-03-31 16:22:05 +0200

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/Context/AuthContext.js

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -7,9 +7,40 @@ const AuthReducer = (state, action) => {
   }
 };
 
+const signUp = (dispatch) =>{
+  return({email , password}) => {
+    // make API request to signUP
+
+    // if we signUp ? mpdify state 
+
+    // if sign up fails , Show  error msg
+
+  }
+}
+const signIn = (dispatch) =>{
+  return({email , password}) => {
+    // make API request to signIn
+
+    // if we signIn ? mpdify state 
+
+    // if sign in fails , Show  error msg
+
+  }
+}
+const signOut = (dispatch) =>{
+  return({email , password}) => {
+    // make API request to signOut
+
+    // if we signOut ? mpdify state 
+
+
+  }
+}
+
+
 export const { Provider, Context } = CreateDataContext(
     AuthReducer,
-     {},
+     {signIn , signOut , signUp},
      {isSignedIn :false}
      
      );
